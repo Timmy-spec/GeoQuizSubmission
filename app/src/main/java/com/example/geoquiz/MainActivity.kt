5539ffc2b785e939afd8b35b48ce741eed330a01
Timmy <timothy.fraser11@gmail.com>
2023-08-24 13:26:17 +1000

    Start

--- app/src/main/java/com/example/geoquiz/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.geoquiz
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import com.example.geoquiz.databinding.ActivityMainBinding
 
@@ -21,6 +24,14 @@ class MainActivity : AppCompatActivity() {
 
     private val quizViewModel: QuizViewModel by viewModels()
 
+    private val cheatLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == Activity.RESULT_OK) {
+            quizViewModel.isCheater =
+                result.data?.getBooleanExtra(EXTRA_ANSWER_SHOWN, false) ?: false
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -41,7 +52,6 @@ class MainActivity : AppCompatActivity() {
             checkAnswer(false)
         }
 
-
         binding.questionTextView.setOnClickListener{
             quizViewModel.moveToNext()
             updateQuestion()
@@ -57,8 +67,12 @@ class MainActivity : AppCompatActivity() {
             updateQuestion()
         }
 
+        binding.cheatButton.setOnClickListener {
+            val answerIsTrue = quizViewModel.currentQuestionAnswer
+            val intent = CheatActivity.newIntent(this@MainActivity, answerIsTrue)
+            cheatLauncher.launch(intent)
+        }
         updateQuestion()
-
     }
 
     override fun onStart() {
@@ -94,10 +108,10 @@ class MainActivity : AppCompatActivity() {
     private fun checkAnswer(userAnswer: Boolean){
         val correctAnswer = quizViewModel.currentQuestionAnswer
 
-        val messageResId = if (userAnswer == correctAnswer){
-            R.string.correct_toast
-        } else {
-            R.string.incorrect_toast
+        val messageResId = when {
+            quizViewModel.isCheater -> R.string.judgment_toast
+            userAnswer == correctAnswer -> R.string.correct_toast
+            else -> R.string.incorrect_toast
         }
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
     }
--- app/src/main/java/com/example/geoquiz/QuizViewModel.kt
+++ app/src/main/java/com/example/geoquiz/QuizViewModel.kt
@@ -8,6 +8,8 @@ private const val TAG = "QuizViewModel"
 
 const val CURRENT_INDEX_KEY = "CURRENT_INDEX_KEY"
 
+const val IS_CHEATER_KEY = "IS_CHEATER_KEY"
+
 class QuizViewModel(private val savedStateHandle: SavedStateHandle): ViewModel() {
 
     private val questionBank = listOf(
@@ -18,6 +20,11 @@ class QuizViewModel(private val savedStateHandle: SavedStateHandle): ViewModel()
         Question(R.string.question_americas, true),
         Question(R.string.question_asia, true)
     )
+
+
+    var isCheater: Boolean
+        get() = savedStateHandle.get(IS_CHEATER_KEY) ?: false
+        set(value) = savedStateHandle.set(IS_CHEATER_KEY, value)
 
     private var currentIndex: Int
         get() = savedStateHandle.get(CURRENT_INDEX_KEY) ?: 0
@@ -37,7 +44,5 @@ class QuizViewModel(private val savedStateHandle: SavedStateHandle): ViewModel()
     fun moveToPrevious () {
         currentIndex = (currentIndex - 1) % questionBank.size
     }
-
-
 
 }
